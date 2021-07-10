00474c07bca59923613989c464722a34c1a1002d
zzarbttoo <qodbwls70@naver.com>
2021-07-10 21:53:54 +0900

    Add GitIgnore and parse api result

---
+++ library/routes/library.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var secret_config = require('../secret.json');
+var library_config = secret_config.library_date;
+const url = library_config.url 
++ "/" + library_config.token 
++ "/" + library_config.type 
++ "/" + library_config.service 
++ "/" + library_config.first_index 
++ "/" + library_config.last_index;
+
+router.get("/date", function(req, res, next){
+
+    console.log(library_config);
+    console.log(url);
+
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200 ){
+            let library_list = JSON.parse(body).SeoulPublicLibraryInfo;
+
+            res.send(library_list);
+
+        }
+    });
+});
+
+
+module.exports = router; //모듈로 송출할 값
+
+
+
+
+
+
