6773ec10530a26034c23224e1d90ef092d1974ab
zzarbttoo <qodbwls70@naver.com>
2021-07-14 23:45:15 +0900

    SELECT 결과 return 수정 필요

--- library/routes/users.js
+++ library/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
 
 var user_db = require('../database/user_db');
 
@@ -7,10 +8,8 @@ var user_db = require('../database/user_db');
 /* GET users listing. */
 router.get('/select', function(req, res, next) {
 
-  
-  console.log(user_db);
-  console.log(user_db.select);
 
+  console.log(user_db.select());
   res.send('respond with a resource');
 });
 
@@ -18,25 +17,63 @@ router.get('/select', function(req, res, next) {
 router.post('/insert', function(req, res, next){
 
   let jsondata = req.body;
-  let values = []
+  let value = [jsondata.user_name, jsondata.user_email, jsondata.user_phone];
 
-  values.push([jsondata.user_name, jsondata.user_email, jsondata.user_phone]);
+  // TODO : 기존 회원 확인 
 
-  console.log(user_db.insert(values));
-  res.send('insert success');
+  user_db.insert(value, next);
+
+   res.send('insert success');
+
+ });
+
+router.put('/update', function(req, res, next){
+
+  let jsondata = req.body;
+  let update_value = [jsondata.user_id, jsondata.user_name, jsondata.user_email, jsondata.user_phone];
+
+  // TODO : 기존 회원 확인 
+
+  // update //email, phone_number만 바꿀 수 있도록 한다 
+  user_db.update(update_value);
+
+
+
+
+
+  
+
+
+
+
+});
+
+router.delete('/delete/:usernum', function(req, res, next){
+
+
+  let usernum = req.params.usernum;
+  //user_name, user_number, user_email -> select user_number
+
+  console.log(usernum);
+  //delete user_number
+  user_db.delete(usernum);
+
 
   
 
 
 });
 
-router.put('/update', function(req, res, next){
+router.get('/selectone/:usernum', function(req, res, next){
+
+  let usernum = req.params.usernum;
+
+  console.log(user_db.select_one(usernum));
+
 
 });
 
-router.delete('/delete', function(req, res, next){
 
 
-});
 
 module.exports = router;
