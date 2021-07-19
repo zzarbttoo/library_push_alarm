39af90aa6f234f9812660000ec436c5824d9d5d3
zzarbttoo <qodbwls70@naver.com>
2021-07-19 14:21:26 +0900

    Add Swagger and sequelize

--- library/app.js
+++ library/app.js
@@ -2,37 +2,40 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var swaggerUi = require('swagger-ui-express');
-var swaggerDocument = require('./utils/swagger');
-var cronjob = require('./utils/cronjob.js');
-
-var logger = require('./utils/logger');
-
-logger.info('logger test');
-
-// cronjob을 여기부터 밑으로 적기
-cronjob.testcron; 
+var logger = require('morgan');
 
 var app = express();
 
+const {swaggerUi, specs} = require('./utils/swagger');
+
+var indexRouter = require('./routes/index');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs),{explorer : true});
+
+
+//sequelize sync method 
+const models = require('./models/index.js');
+
+models.sequelize.sync().then(() => {
+  console.log("DB Connect Success");
+
+}).catch(err => {
+  console.log("DB Connect Fail"); 
+  console.log(err);
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
-//app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,5 +56,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-
